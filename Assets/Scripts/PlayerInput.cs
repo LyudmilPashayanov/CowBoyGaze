b43b7eb5ddf52da246e3192a07cbdd4b28f97763
Stanislavov,David D <d.stanislavov@student.fontys.nl>
2019-02-21 12:50:01 +0200

    Gaze working

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class PlayerInput : MonoBehaviour
+{
+    private void Select()
+    {
+        return;
+    }
+
+    private GazePoint Hover()
+    {
+        return TobiiAPI.GetGazePoint();
+    }
+
+	void Update ()
+    {
+	}
+}
