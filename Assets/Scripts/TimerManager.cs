bea7af1c0e26f667ff5fe980b8e0b13b29037f9d
Stanislavov,David D <d.stanislavov@student.fontys.nl>
2019-03-18 14:11:45 +0200

    Added basic timer

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    private float _timer;
+
+    public float timerDuration = 60;
+    public Text timerText;
+
+	void Start ()
+    {
+        this._timer = timerDuration;
+	}
+	
+	void Update ()
+    {
+        this._timer -= Time.deltaTime;
+        this.timerText.text = this._timer.ToString("F2");
+	}
+}
