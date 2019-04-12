6373e58a3a2684fd0b10e9476e1d613eb4b18061
Stanislavov,David D <d.stanislavov@student.fontys.nl>
2019-04-12 18:03:02 +0300

    Menu item prefab added

---
+++ Assets/Scripts/SelectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+public class SelectController : MonoBehaviour
+{
+    private GazeAware _gazeAware;
+
+    void Start ()
+    {
+        this._gazeAware = GetComponent<GazeAware>();
+    }
+	
+	void Update ()
+    {
+		if (this._gazeAware.HasGazeFocus)
+        {
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x + Time.deltaTime, this.gameObject.transform.localScale.y + Time.deltaTime, 1);
+        }
+        else if (!this._gazeAware.HasGazeFocus && this.gameObject.transform.localScale.x > 1)
+        {
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x - Time.deltaTime, this.gameObject.transform.localScale.y - Time.deltaTime, 1);
+        }
+	}
+}
